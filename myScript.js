e0751aea6701f904e33afde421e280648ba3e2ed
Anne Gitau <anne.gitau44@gmail.com>
2016-10-20 10:42:12 +0000

    on Watch added

--- myScript.js
+++ myScript.js
@@ -1,7 +1,8 @@
 (function () {
     
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-    var pictureSource, destinationType;
+    var pictureSource;
+	var destinationType;
     
     function onDeviceReady() {
         pictureSource = navigator.camera.PictureSourceType;
@@ -10,33 +11,65 @@
         document.getElementById("capturePhoto").onclick = function () {
             alert("Am here!");
             navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
-                quality : 50, destinationType : destinationType.DATA_URL
-            });
+                quality : 50,
+				destinationType : destinationType.DATA_URL});
         }
-        document.getElementById("geolocationdata")addEventListener("click",function(){
+        cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com",
+			function(success) {
+				alert("encode success: " + success);
+				},
+			function(fail) {
+				alert("encoding failed: " + fail);
+			});
+		document.getElementById("geolocationdata")addEventListener("click",function(){
             navigator.geolocation.getCurrentLocation(onSuccess, onError, {enableHighAccuracy:true});
         });
+		var watchID = navigator.geolocation.watchPosition(onSuccess, onError, {
+			timeout: 30000
+			});
+		document.getElementById("clearWatchbtn")addEventListener("click",function(){
+			navigator.geolocation.clearWatch(watchID);
+		});
     };
-    var onSuccess = function(position){
-        alert('Latitude' + position.coords.latitude + '\n'+
-             'Longitute' + position.coords.longitude + '\n');
-    };
-    function onError(error){
-        alert('code'+error.code + '\n'+
-             'Message : '+ error.message + '\n');
-    }
-     function onPhotoDataSuccess(imageData){
+			  
+	// onSuccess Callback 
+    //   This method accepts a `Position` object, which contains 
+    //   the current GPS coordinates 
+     
+    var onSuccess = function(position) {
+		alert('Latitude: ' + position.coords.latitude + '\n' + 'Longitude: ' + position.coords.longitude + '\n');
+	};
+ 
+    // onError Callback receives a PositionError object 
+    // 
+    function onError(error) {
+		alert('code: ' + error.code + '\n' +
+			  'message: ' + error.message + '\n');
+	}
+	var onWatchSuccess = function(position) {
+		var element = document.getElementById('divWatchMeMove');
+		element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' + 'Longitude: ' + position.coords.longitude + '<br />' + '<hr />' + element.innerHTML;
+	};
+
+	function onWatchError(error) {
+		alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+	}
+ 
+    // Options: throw an error if no update is received every 30 seconds. 
+    // 
+	function onPhotoDataSuccess(imageData){
          var smallImage = document.getElementById('smallImage');
          smallImage.style.display = 'block';
          smallImage.src = "data:image/jpeg;base64," + imageData;
-     }
+     };
     function onFail(message){
         alert("Failed because" + message);
-    }
-    
-    /*function onPhotoURLSuccess(imageURL){
+    };
+	   
+    function onPhotoURLSuccess(imageURL){
          var largeImage = document.getElementById('largeImage');
          largeImage.style.display = 'block';
          largeImage.src = "data:image/jpeg;base64," + imageURL;
-     };*/
+     };
+	 
 })();
